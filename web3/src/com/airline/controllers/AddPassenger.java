088d1669317aefec20f93e9a3592f4e7fd7d1d67
samsungg <samsungg@samsung>
2018-02-17 17:18:43 +0200

    start web3

---
+++ web3/src/com/airline/controllers/AddPassenger.java
@@ -0,0 +1,54 @@
+package com.airline.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddPassenger
+ */
+@WebServlet("/AddPassenger")
+public class AddPassenger extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddPassenger() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		/*PrintWriter out = response.getWriter();
+		
+		response.setContentType("text/html");
+		
+		out.println("<html><body>");
+		out.println("<h2>welcome</h2>");
+		out.println("</html></body>");*/
+		
+		RequestDispatcher view=request.getRequestDispatcher("WEB-INF/views/addPassenger.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
